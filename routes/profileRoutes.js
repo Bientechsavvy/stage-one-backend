b668af92d2819c6a3038e751d12cb9147fb8152c
Bientechsavvy <olawalesossagbetibienvenu@gmail.com>
2026-04-17 01:26:00 +0100

    Update profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/profileController");
+const profileController = require("../controllers/profileController");
 
-router.post("/profiles", controller.createProfile);
+// CREATE PROFILE
+router.post("/profiles", profileController.createProfile);
+
+// GET ALL PROFILES
+router.get("/profiles", profileController.getAllProfiles);
+
+// GET ONE PROFILE
+router.get("/profiles/:id", profileController.getProfileById);
+
+// DELETE PROFILE
+router.delete("/profiles/:id", profileController.deleteProfile);
 
 module.exports = router;
